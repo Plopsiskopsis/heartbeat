ec23b3b4304cc426c4ce6336f2d71a89dc3de7c5
Plopsis <kirvesmies70@hotmail.com>
2021-02-17 19:44:46 +0200

    Death and etc collection

--- game/3D_level/Player.gd
+++ game/3D_level/Player.gd
@@ -8,7 +8,6 @@ var velocity :Vector3 = Vector3()
 var direction :Vector3 = Vector3()
 var asekohta :Vector3 = Vector3(0,0.3,-0.7)
 var gravity :float = -9.8 * 3
-var flying :bool = false
 var es_drinks : int = 0
 var bpm :float = 80.0
 
@@ -17,25 +16,24 @@ const MAX_RUNNING_SPEED :float = 30.0
 const ACCEL :float = 2.0
 const DEACCEL :float = 6.0
 
-const FLY_SPEED :float = 20.0
-const FLY_ACCEL :float = 2.0
-
 var jump_height :float = 15.0
 
-func _process(delta):
-	$UI
-
-func _ready():
+func _ready() -> void:
 	Global.player = self
 	$UI.heartbeat()
 	$heart_timer.wait_time = 60.0 / bpm
 
 func _physics_process(delta) -> void:
-	if flying:
-		fly(delta)
-	else:
 		walk(delta)
-	
+
+func _process(delta):
+	bpm -= delta
+	if bpm < 50.0 or bpm > 200.0:
+		print("You dead")
+		get_tree().change_scene_to(load("res://Menu/Menu.tscn"))
+
+func set_cam() -> void:
+	cam.current = true
 
 func check_drinks() -> void:
 	if es_drinks >= 0:
@@ -45,6 +43,7 @@ func _input(event) -> void:
 	if Input.is_action_just_pressed("drink"):
 		if es_drinks > 0:
 			es_drinks -= 1
+			$UI.es_count(es_drinks)
 			$AnimationPlayer.play("drink")
 			bpm += 10
 			$heart_timer.wait_time = 60.0 / bpm
@@ -56,8 +55,6 @@ func _input(event) -> void:
 			cam.rotate_x(deg2rad(change))
 			camera_angle += change
 	
-	if Input.is_action_just_pressed("ui_focus_next"):
-		flying = !flying
 	
 	if Input.is_action_just_pressed("action"):
 		var ray_length :float = 100.0
@@ -73,6 +70,7 @@ func _input(event) -> void:
 						result.collider.get_parent().action() 
 						if result.collider.is_in_group("es"):
 							es_drinks += 1
+							$UI.es_count(es_drinks)
 							if es_drinks > 0:
 								$head/Camera/es.visible = true
 					
@@ -111,23 +109,6 @@ func walk(delta) -> void:
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_height
-
-func fly(delta) -> void:
-	direction = Vector3()
-	var aim :Basis = $head/Camera.get_global_transform().basis
-	if Input.is_action_pressed("move_forward"):
-		direction -= aim.z
-	if Input.is_action_pressed("move_backward"):
-		direction += aim.z
-	if Input.is_action_pressed("move_left"):
-		direction -= aim.x
-	if Input.is_action_pressed("move_right"):
-		direction += aim.x
-	direction = direction.normalized() 
-	var target :Vector3 = direction * FLY_SPEED
-	velocity = velocity.linear_interpolate(target,FLY_ACCEL * delta)
-# warning-ignore:return_value_discarded
-	move_and_slide(velocity)
 
 
 func _on_heart_timer_timeout() -> void:
--- game/UI/UI.gd
+++ game/UI/UI.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func es_count(count):
+	$ui_es/Label.text = str(count)
+
 func heartbeat() -> void:
 	$Heart.beat()
 	$Heart/Label.text = str(Global.player.bpm) 
