ac7ac75e2358cc0b1961c74106e47a70ed34ddeb
mjarvihaavisto <60304828+mjarvihaavisto@users.noreply.github.com>
2021-02-21 17:55:37 +0200

    Muokattu tarinaa

--- game/Menu/Lose_screen.gd
+++ game/Menu/Lose_screen.gd
@@ -6,9 +6,9 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	match Global.death_reason:
 		"Too much":
-			reason_label.text = "You had too much ES and your heart exploded!"
+			reason_label.text = "Jonne had too much ES and had a heart attack!"
 		"Too little":
-			reason_label.text = "You had too little ES and you fell asleep!"
+			reason_label.text = "Jonne had too little ES and fell asleep!"
 
 func _on_TextureButton_pressed():
 # warning-ignore:return_value_discarded
